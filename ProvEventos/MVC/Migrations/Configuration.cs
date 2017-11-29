161873697d2a1b3a0d1f8ed6e81ad5cb24080fdb
yalvarez <yalvarez@hg.com.uy>
2017-11-29 03:12:18 -0300

    archivos ok

--- ProvEventos/MVC/Migrations/Configuration.cs
+++ ProvEventos/MVC/Migrations/Configuration.cs
@@ -28,31 +28,7 @@ namespace MVC.Migrations
             //    );
             //
 
-
-
-            context.Provinces.AddOrUpdate(x => x.Id,
-            new Models.Province { Id = 1, Name = "Artigas" },
-            new Models.Province { Id = 2, Name = "Canelones" },
-            new Models.Province { Id = 3, Name = "Cerro Largo" },
-            new Models.Province { Id = 4, Name = "Colonia" },
-            new Models.Province { Id = 5, Name = "Durazno" },
-            new Models.Province { Id = 6, Name = "Flores" },
-            new Models.Province { Id = 7, Name = "Florida" },
-            new Models.Province { Id = 8, Name = "Lavalleja" },
-            new Models.Province { Id = 9, Name = "Maldonado" },
-            new Models.Province { Id = 10, Name = "Montevideo" },
-            new Models.Province { Id = 11, Name = "Paysandú" },
-           new Models.Province { Id = 12, Name = "Río Negro" },
-           new Models.Province { Id = 13, Name = "Rivera" },
-           new Models.Province { Id = 14, Name = "Rocha" },
-           new Models.Province { Id = 15, Name = "Salto" },
-           new Models.Province { Id = 16, Name = "San José" },
-           new Models.Province { Id = 17, Name = "Soriano" },
-           new Models.Province { Id = 18, Name = "Tacuarembó" },
-           new Models.Province { Id = 19, Name = "Treinta y Tres" });
-
-
-           context.SaveChanges();
+            
         }
     }
 }
